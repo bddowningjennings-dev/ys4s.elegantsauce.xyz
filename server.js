7394b86ac661c376789fb2842cb85aebd976fdcd
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2018-11-19 03:36:08 -0700

    fix issue with static dir routing after build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').load()
 const
-  PORT = 5001,
-  DATABASE = 'ys4s-skipio',
+  PORT = 8080,
+  DATABASE = 'react-ys4s',
   bodyParser = require('body-parser'),
   express = require('express'),
   app = express()
@@ -10,7 +10,7 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/build/index.html'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true, limit: '25mb'}));
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/build'))
 
 require('./server/config/mongoose')(DATABASE)
 require('./server/config/routes')(app)
